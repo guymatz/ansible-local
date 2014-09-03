50d1b2be3e2547476b976c4f3759028c7296724f
Guy Matz <gmatz@mdsol.com>
2014-09-03 15:11:36 -0400

    adding checkout of dev ansible code from ansible

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd $(dirname $0)
 git pull
+[ -f /opt/ansible/hacking/env-setup ] && source /opt/ansible/hacking/env-setup > /dev/null 2>&1
 ansible-playbook -i inventory/local site.yml $*
-cd -
+cd - > /dev/null 2>&1
