d8be67812391b8bfe14758ad19e06388bf3cda30
Guy Matz <gmatz@mdsol.com>
2014-10-03 17:49:12 -0400

    latest

--- run_ansible.sh
+++ run_ansible.sh
@@ -2,5 +2,6 @@
 cd $(dirname $0)
 git pull
 [ -f /opt/ansible/hacking/env-setup ] && source /opt/ansible/hacking/env-setup > /dev/null 2>&1
-ansible-playbook -i inventory/aws ipython.yml $*
+#ansible-playbook -i inventory/aws ipython.yml $*
+ansible-playbook -i inventory/local local.yml $*
 cd - > /dev/null 2>&1
