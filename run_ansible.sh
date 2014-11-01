0fcb040b19b2e3a62d72e105619addecb1c8cb57
Guy Matz <gmatz@mdsol.com>
2014-11-01 11:53:03 -0400

    someone says git pull --rebase is better

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 cd $(dirname $0)
-git pull
+git pull --rebase
 [ -f /opt/ansible/hacking/env-setup ] && source /opt/ansible/hacking/env-setup > /dev/null 2>&1
+which ansible-playbook
 #ansible-playbook -i inventory/aws ipython.yml $*
 ansible-playbook -i inventory/local local.yml $*
 cd - > /dev/null 2>&1
