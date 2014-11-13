dc2ccada519e6d6b2a23bcf65e9e7c88a984e3e3
Guy Matz <gmatz@mdsol.com>
2014-11-13 17:38:40 -0500

    running local

--- run_ansible.sh
+++ run_ansible.sh
@@ -4,5 +4,5 @@ git pull --rebase
 [ -f /opt/ansible/hacking/env-setup ] && source /opt/ansible/hacking/env-setup > /dev/null 2>&1
 which ansible-playbook
 #ansible-playbook -i inventory/aws ipython.yml $*
-ansible-playbook -i inventory/local local.yml $*
+ansible-playbook -i inventory/local local.yml --connection local $*
 cd - > /dev/null 2>&1
