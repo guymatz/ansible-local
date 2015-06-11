b499fcb9d250106ec41272de804b31d6e735a273
Guy Matz <gmatz@jcrew.com>
2015-06-11 17:32:34 -0400

    lots of changes.  added ruby role

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd $(dirname $0)
-git pull --rebase
+#git pull --rebase
 [ -f /opt/ansible/hacking/env-setup ] && source /opt/ansible/hacking/env-setup > /dev/null 2>&1
 which ansible-playbook
 #ansible-playbook -i inventory/aws ipython.yml $*
