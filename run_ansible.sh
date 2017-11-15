368f7f976e3d0e801e2c2177f5c3462a32cf9107
Guy Matz <guy.matz@groupm.com>
2017-11-15 11:46:26 -0500

    latest

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-#cd $(dirname $0)
-#git pull --rebase
-[ -f /opt/ansible/hacking/env-setup ] && source /opt/ansible/hacking/env-setup > /dev/null 2>&1
+ANSIBLE_ROOT=~/Code/ansible
+cd $ANSIBLE_ROOT
+git pull --rebase
+source venv/bin/activate
+[ -f $ANSIBLE_ROOT/hacking/env-setup ] && source $ANSIBLE_ROOT/hacking/env-setup > /dev/null 2>&1
 #which ansible-playbook
 #ansible-playbook -i inventory/aws ipython.yml $*
+cd -
 ansible-playbook -i inventory/local local.yml --connection local $*
 cd - > /dev/null 2>&1
